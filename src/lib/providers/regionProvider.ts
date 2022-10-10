ae97e899c3a16554688d7c613a62b5412633e738
gozolana <gozolana@gmail.com>
2022-10-10 17:49:07 +0900

    Add findRegionOfZone

--- src/lib/providers/regionProvider.ts
+++ src/lib/providers/regionProvider.ts
@@ -1,21 +1,29 @@
-import { Region, RegionImpl } from "../entities/region";
-import { regionData } from "../resources/zones.data";
+import { Region, RegionImpl } from '../entities/region';
+import { regionData } from '../resources/zones.data';
 
 class RegionProvider {
   constructor() {
     this.huntRegions = regionData.huntRegions.map((hr) => new RegionImpl(hr));
-    this.weatherRegions = regionData.weatherRegions.map((hr) => new RegionImpl(hr));
+    this.weatherRegions = regionData.weatherRegions.map(
+      (hr) => new RegionImpl(hr)
+    );
   }
 
   readonly huntRegions: Region[];
   readonly weatherRegions: Region[];
 
+  findRegionOfZone(zoneId: number): Region | undefined {
+    return this.weatherRegions.find((r) =>
+      r.zones.map((zone) => zone.id).includes(zoneId)
+    );
+  }
+
   getHuntRegions(): Region[] {
-    return this.huntRegions
+    return this.huntRegions;
   }
 
   getWeatherRegions(): Region[] {
-    return this.weatherRegions
+    return this.weatherRegions;
   }
 }
 
