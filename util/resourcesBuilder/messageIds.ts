1b6c75e2113e8738405d71009842f6604da35aa0
gozolana <gozolana@gmail.com>
2024-01-01 18:41:31 +0900

    Remove old resource builder code

--- util/resourcesBuilder/messageIds.ts
+++
@@ -1,8 +0,0 @@
-interface MessageIds {
-  bNpcNameIdSet: Set<number>;
-  placeNameIdSet: Set<number>;
-  placeNameIdToZoneId: Map<number, number>;
-  weatherIdSet: Set<number>;
-}
-
-export { MessageIds };
