75cca142a5021aa121aa7b7d4c2292be30cf3ca8
gozolana <gozolana@gmail.com>
2024-06-15 18:07:45 +0900

    format

--- src/compare.spec.ts
+++ src/compare.spec.ts
@@ -1,40 +1,40 @@
 interface Item {
-  propA: string;
-  propB: string;
+  propA: string
+  propB: string
 }
 
 class ItemImpl implements Item {
-  propA: string;
-  propB: string;
+  propA: string
+  propB: string
   constructor(propA: string, propB: string) {
-    this.propA = propA;
-    this.propB = propB;
+    this.propA = propA
+    this.propB = propB
   }
   compare = (another: Item): number =>
     this.propA.localeCompare(another.propA) ||
-    this.propB.localeCompare(another.propB);
+    this.propB.localeCompare(another.propB)
 }
 
 function sum(a: number, b: number) {
-  return a + b;
+  return a + b
 }
-import { expect, test } from "vitest";
+import { expect, test } from 'vitest'
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
-});
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
+})
 
 test.each([
-  ["aaa", "bbb", "=", "aaa", "bbb"],
-  ["aaa", "bbc", ">", "aaa", "bbb"],
-  ["aaa", "bbb", "<", "aaa", "bbc"],
-  ["aab", "bbb", ">", "aaa", "bbb"],
-  ["aaa", "bbb", "<", "aab", "bbb"],
-  ["aab", "bbb", ">", "aaa", "bbc"],
-  ["aaa", "bbc", "<", "aab", "bbb"],
+  ['aaa', 'bbb', '=', 'aaa', 'bbb'],
+  ['aaa', 'bbc', '>', 'aaa', 'bbb'],
+  ['aaa', 'bbb', '<', 'aaa', 'bbc'],
+  ['aab', 'bbb', '>', 'aaa', 'bbb'],
+  ['aaa', 'bbb', '<', 'aab', 'bbb'],
+  ['aab', 'bbb', '>', 'aaa', 'bbc'],
+  ['aaa', 'bbc', '<', 'aab', 'bbb'],
 ])(`compareテスト %s.%s %s %s.%s`, async (a1, a2, sign, b1, b2) => {
-  const itemA = new ItemImpl(a1, a2);
-  const itemB = new ItemImpl(b1, b2);
-  const result = sign === ">" ? 1 : sign === "<" ? -1 : 0;
-  expect(itemA.compare(itemB)).toBe(result);
-});
+  const itemA = new ItemImpl(a1, a2)
+  const itemB = new ItemImpl(b1, b2)
+  const result = sign === '>' ? 1 : sign === '<' ? -1 : 0
+  expect(itemA.compare(itemB)).toBe(result)
+})
--- src/lib/entities/spawnPeriod.ts
+++ src/lib/entities/spawnPeriod.ts
@@ -1,14 +1,14 @@
-import { EorzeaPeriod } from './eorzeaPeriod';
+import { EorzeaPeriod } from './eorzeaPeriod'
 
 export class SpawnPeriod extends EorzeaPeriod {
-  readonly subPeriods: EorzeaPeriod[];
+  readonly subPeriods: EorzeaPeriod[]
 
   constructor(
     startTimeStamp: number,
     endTimeStamp: number,
     subPeriods: EorzeaPeriod[]
   ) {
-    super(startTimeStamp, endTimeStamp);
-    this.subPeriods = subPeriods;
+    super(startTimeStamp, endTimeStamp)
+    this.subPeriods = subPeriods
   }
 }
